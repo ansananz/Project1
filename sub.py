8cf0f83deb1b7467b58f57c4d121a2cc1590f9c7
ansananz <22bcae12@kristujayanti.com>
2024-01-30 14:09:35 +0530

    first commit

---
+++ sub.py
@@ -0,0 +1,6 @@
+# Subtraction of two numbers
+#take two user input number and subtract them
+num1=int(input("enter 1st integer no."))
+num2=int(input("enter 2nd integer no."))
+sub = num1 - num2
+print("the Subtraction of",num1 ,"and",num2 ,"are", sub)
